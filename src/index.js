eced7eb0e9a4c376713cf7b8daa6f153eda5c4b6
Leonardo Vivanco <leorvivanco@gmail.com>
2018-09-10 19:49:50 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import ViddeoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyBr7W4n3f27uG1vtfNDU6Q_it9lMd2p2Jk';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {videos: []};
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos})
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <ViddeoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDom.render(<App />, document.querySelector('.container'));
+
+
+
+/*Estrutura do React
+Criar pasta componentes
+Criar cada componentes
+    Home, menu, preços, carros...
+Importar API na index
+
+
+Css
+
+Criar padrão
+
+
+
+*/
